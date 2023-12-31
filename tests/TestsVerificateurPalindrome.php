29e705a816afc74a60feca43e7722c41ab17cd3a
Ayoub-Tehari <ayoub.tehari01@gmail.com>
2023-12-31 16:29:33 +0100

    refactor cycle 1

--- tests/TestsVerificateurPalindrome.php
+++ tests/TestsVerificateurPalindrome.php
@@ -7,18 +7,23 @@ class TestsVerificateurPalindrome extends TestCase
     //inputs
 
 
+    const INPUTS = array("palindromes" => array("radar", "abba"),
+                    "autres" => array("test", "palindrome"));
     /*
         QUAND on saisit une chaîne
         ALORS celle-ci est renvoyée en miroir
     */
     public function testMiroireDePalindrome () {
-        $inputs = array("palindromes" => array("radar", "abba"),
-                            "autres" => array("test", "palindrome"));
 
         $verificateur = new VerificateurPalindrome();
-        foreach($inputs["palindromes"] as $data){
+        foreach(self::INPUTS["palindromes"] as $data){
             $resultat = $verificateur->renverser($data);
             $this->assertEquals($resultat, $data);
         }
     }
+    /*
+        QUAND on saisit un palindrome 
+        ALORS celui-ci est renvoyé 
+        ET « Bien dit » est envoyé ensuite
+    */
 }
