0da118bae751a41eba8fcb6186a320d030c1c796
Ayoub-Tehari <ayoub.tehari01@gmail.com>
2023-12-31 16:49:18 +0100

    Cycle 2 : etape 1

--- tests/TestsCycle2.php
+++ tests/TestsCycle2.php
@@ -2,23 +2,25 @@
 
 namespace User\MtExeciceKata1;
 use PHPUnit\Framework\TestCase;
-class TestsVerificateurPalindrome extends TestCase
+class TestsCycle2 extends TestCase
 {
     //inputs
 
 
     const INPUTS = array("palindromes" => array("radar", "abba"),
                     "autres" => array("test", "palindrome"));
+    
     /*
-        QUAND on saisit une chaîne
-        ALORS celle-ci est renvoyée en miroir
+        QUAND on saisit un palindrome 
+        ALORS celui-ci est renvoyé 
+        ET « Bien dit » est envoyé ensuite
     */
-    public function testMiroireDePalindrome () {
+    public function testPalindromeEtBienDit () {
 
         $verificateur = new VerificateurPalindrome();
         foreach(self::INPUTS["palindromes"] as $data){
-            $resultat = $verificateur->renverser($data);
-            $this->assertEquals($resultat, $data);
+            $resultat = $verificateur->epilog($data);
+            $this->assertEquals($resultat, $data.PHP_EOL."Bien dit" );
         }
     }
     
